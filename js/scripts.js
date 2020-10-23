7258908f744b5d5569c0db62cdc2c8088586e8e7
Joey3001 <josephnero111@gmail.com>
2020-10-23 10:25:13 -0700

    Create js to take user input from radio buttons and store it in a variable called 'color'.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
   $("#languageForm").submit(function(){
     const name = $("input#name").val();
+    const color = $("input:radio[name=color]:checked").val();
     alert(name);
+    alert(color)
   })
 })
