3f1b12ef5fcf434ada7817a48f7e139d77560a2a
Joey3001 <josephnero111@gmail.com>
2020-10-23 11:38:05 -0700

    Add logic to js that provides the user an alert if they don't answer at least 1 of the questions.

--- js/scripts.js
+++ js/scripts.js
@@ -6,24 +6,23 @@ $(document).ready(function(){
     const animal = $("#animal").val()
     const food = $("#food").val()
     const weather = $("#weather").val()
-    alert(name);
-    alert(color);
-    alert(activity);
-    alert(animal);
-    alert(food);
-    alert(weather);
 
-    if (color === "blue" && activity ==="1") {
-      alert("this if statement is functional");
-    }
-    else if (color === "yellow" || food === "3") {
-      alert("your color was yellow or your favorite food was French Fries");
-    }
-    else if (animal === "4") {
-      alert("your color and your activity were not simultaneously blue & singing, your color was not yellow, your favorite food was not french fries, and your favorite animal was a gezelle");
+    if (name || color || activity || animal || food || weather){
+      if (color === "blue" && activity ==="1") {
+        alert("this if statement is functional");
+      }
+      else if (color === "yellow" || food === "3") {
+        alert("your color was yellow or your favorite food was French Fries");
+      }
+      else if (animal === "4") {
+        alert("your color and your activity were not simultaneously blue & singing, your color was not yellow, your favorite food was not french fries, and your favorite animal was a gezelle");
+      }
+      else {
+        alert("you didnt select any of my things i programmed");
+      }
     }
     else {
-      alert("you didnt select any of my things i programmed");
+      alert("you must answer at least one of the questions, please try again");
     }
   })
 })
