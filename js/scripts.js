cd47ef3fd4b38c0d92f230874b348a972e96eb0b
Joey3001 <josephnero111@gmail.com>
2020-10-23 10:41:38 -0700

    Create a select menu and js to take user input from the menu and store it in a variable called 'activity'.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,9 @@ $(document).ready(function(){
   $("#languageForm").submit(function(){
     const name = $("input#name").val();
     const color = $("input:radio[name=color]:checked").val();
+    const activity = $("#activity").val()
     alert(name);
-    alert(color)
+    alert(color);
+    alert(activity);
   })
 })
