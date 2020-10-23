73dbf9b6463a436f3891c9e5b2415af587648e09
Joey3001 <josephnero111@gmail.com>
2020-10-23 10:53:39 -0700

    Add additional select menu's and js to take user input and store them in variables.

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,14 @@ $(document).ready(function(){
     const name = $("input#name").val();
     const color = $("input:radio[name=color]:checked").val();
     const activity = $("#activity").val()
+    const animal = $("#animal").val()
+    const food = $("#food").val()
+    const weather = $("#weather").val()
     alert(name);
     alert(color);
     alert(activity);
+    alert(animal)
+    alert(food)
+    alert(weather)
   })
 })
