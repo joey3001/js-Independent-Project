f36b60f324b468b02506cda814749cf7deb5c7c0
Joey3001 <josephnero111@gmail.com>
2020-10-23 15:27:39 -0700

    Add logic to call the user 'user' in the generated responses if they do not enter a name.

--- js/scripts.js
+++ js/scripts.js
@@ -7,24 +7,52 @@ $(document).ready(function(){
     const food = $("#food").val()
     const weather = $("#weather").val()
     $("#content div").hide();
-    $(".user").text(name);
-    
-    if (color || activity || animal || food || weather){
-      if (color === "blue" && activity ==="1") {
-        $("#ruby").show();
-      }
-      else if (color === "yellow" || food === "3") {
-        $("#javaScript").show();
-      }
-      else if (animal === "4") {
-        $("#python").show();
+
+    if(name){
+      if (color || activity || animal || food || weather){
+        if (color === "blue" && activity ==="1") {
+          $("#ruby").show();
+          $(".user").text(name);
+        }
+        else if (color === "yellow" || food === "3") {
+          $("#javaScript").show();
+          $(".user").text(name);
+        }
+        else if (animal === "4") {
+          $("#python").show();
+          $(".user").text(name);
+        }
+        else {
+          $("#swift").show();
+          $(".user").text(name);
+        }
       }
       else {
-        $("#swift").show();
+        alert("you must 1 answer at least one of the questions, please try again");
       }
     }
     else {
-      alert("you must answer at least one of the questions, please try again");
+      if (color || activity || animal || food || weather){
+        if (color === "blue" && activity ==="1") {
+          $("#ruby").show();
+          $(".user").text("User, you didn't enter a name so we will call you User");
+        }
+        else if (color === "yellow" || food === "3") {
+          $("#javaScript").show();
+          $(".user").text("User, you didn't enter a name so we will call you User");
+        }
+        else if (animal === "4") {
+          $("#python").show();
+          $(".user").text("User, you didn't enter a name so we will call you User");
+        }
+        else {
+          $("#swift").show();
+          $(".user").text("User, you didn't enter a name so we will call you User");
+        }
+      }
+      else {
+        alert("you must 2 answer at least one of the questions, please try again");
+      }
     }
     event.preventDefault();
   })
