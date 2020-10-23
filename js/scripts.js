25e72325ffec4609026a7fa9b7399ae9b885eb63
Joey3001 <josephnero111@gmail.com>
2020-10-23 11:52:07 -0700

    Create classes in css & logic in js to show classes based on user input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $("#languageForm").submit(function(){
+  $("#languageForm").submit(function(event){
     const name = $("input#name").val();
     const color = $("input:radio[name=color]:checked").val();
     const activity = $("#activity").val()
@@ -7,18 +7,20 @@ $(document).ready(function(){
     const food = $("#food").val()
     const weather = $("#weather").val()
 
-    if (name || color || activity || animal || food || weather){
+    event.preventDefault();
+
+    if (color || activity || animal || food || weather){
       if (color === "blue" && activity ==="1") {
-        alert("this if statement is functional");
+        $("#ruby").show();
       }
       else if (color === "yellow" || food === "3") {
-        alert("your color was yellow or your favorite food was French Fries");
+        $("#javaScript").show();
       }
       else if (animal === "4") {
-        alert("your color and your activity were not simultaneously blue & singing, your color was not yellow, your favorite food was not french fries, and your favorite animal was a gezelle");
+        $("#python").show();
       }
       else {
-        alert("you didnt select any of my things i programmed");
+        $("#swift").show();
       }
     }
     else {
