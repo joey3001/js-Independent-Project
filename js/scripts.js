da0b6611ab787fa9bd64d2a02a0a2616e34d8ea4
Joey3001 <josephnero111@gmail.com>
2020-10-23 17:38:50 -0700

    Update logic in javascript to achieve the same result with less lines of code.

--- js/scripts.js
+++ js/scripts.js
@@ -7,50 +7,35 @@ $(document).ready(function(){
     const food = $("#food").val()
     const weather = $("#weather").val()
     $("#content div").hide();
-
-    //If you do not enter a name, the program will call you user in the generated response.
-    //If you do enter a name, the program will call you by your entered name in the generated response. 
-    //In either case, if you do not answer at least one of the questions, the program will give you an alert asking you to go back and answer one. 
-    if(name){
-      if (color || activity || animal || food || weather){
-        $(".user").text(name);
-        if (color === "blue" && activity ==="1") {
-          $("#ruby").show();
-        }
-        else if (color === "yellow" || food === "3") {
-          $("#javaScript").show();
-        }
-        else if (animal === "4") {
-          $("#python").show();
-        }
-        else {
-          $("#swift").show();
-        }
-      }
-      else {
-        alert("you must answer at least one of the questions, please try again");
-      }
-    }
-    else {
-      if (color || activity || animal || food || weather){
-        $(".user").text("User, you didn't enter a name so we will call you User");
-        if (color === "blue" && activity ==="1") {
-          $("#ruby").show();
-        }
-        else if (color === "yellow" || food === "3") {
-          $("#javaScript").show();
-        }
-        else if (animal === "4") {
-          $("#python").show();
-        }
-        else {
-          $("#swift").show();
-        }
-      }
-      else {
-        alert("you must answer at least one of the questions, please try again");
-      }
-    }
     event.preventDefault();
+    //If you do not answer at least one of the questions, the program will give you an alert asking you to go back and answer one. 
+    //If you do enter a name, the program will call you by your entered name in the generated response. 
+    //If you do not enter a name, the program will call you 'User' in the generated responses. 
+      if (color || activity || animal || food || weather){
+        if(name){
+          $(".user").text(name);
+        }
+        else{
+          $(".user").text("User, you didn't enter a name so we will call you User");
+        }
+        if (color === "blue" && activity === "1") {
+          $("#ruby").show();
+        }
+        else if (color === "yellow" || food === "3") {
+          $("#javaScript").show();
+        }
+        else if (animal === "4") {
+          $("#python").show();
+        }
+        else {
+          $("#swift").show();
+        }
+      }
+      else {
+        alert("you must answer at least one of the questions, please try again");
+      }
   })
 })
+
+
+
