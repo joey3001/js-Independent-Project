a2eb35c8d3ce5a51761d837a8325a45cc26d148a
Joey3001 <josephnero111@gmail.com>
2020-10-25 19:54:52 -0700

    Adds images and the fadeIn method to generated responses.

--- js/scripts.js
+++ js/scripts.js
@@ -7,33 +7,33 @@ $(document).ready(function(){
     const food = $("#food").val()
     const weather = $("#weather").val()
     $("#content div").hide();
-    event.preventDefault();
     //If you do not answer at least one of the questions, the program will give you an alert asking you to go back and answer one. 
     //If you do enter a name, the program will call you by your entered name in the generated response. 
     //If you do not enter a name, the program will call you 'User' in the generated responses. 
-      if (color || activity || animal || food || weather){
-        if(name){
-          $(".user").text(name);
-        }
-        else{
-          $(".user").text("User, you didn't enter a name so we will call you User");
-        }
-        if (color === "blue" && activity === "1") {
-          $("#ruby").show();
-        }
-        else if (color === "yellow" || food === "3") {
-          $("#javaScript").show();
-        }
-        else if (animal === "4") {
-          $("#python").show();
-        }
-        else {
-          $("#swift").show();
-        }
+    if (color || activity || animal || food || weather){
+      if(name){
+        $(".user").text(name);
+      }
+      else{
+        $(".user").text("User, you didn't enter a name so we will call you User");
+      }
+      if (color === "blue" && activity === "1") {
+        $("#ruby").fadeIn("slow");
+      }
+      else if (color === "yellow" || food === "3") {
+        $("#javaScript").fadeIn("slow");
+      }
+      else if (animal === "4") {
+        $("#python").fadeIn("slow");
       }
       else {
-        alert("you must answer at least one of the questions, please try again");
+        $("#swift").fadeIn("slow");
       }
+    }
+    else {
+      alert("you must answer at least one of the questions, please try again");
+    }
+    event.preventDefault();
   })
 })
 
